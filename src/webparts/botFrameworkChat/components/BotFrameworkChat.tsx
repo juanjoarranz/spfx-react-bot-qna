995b174803b01d8245d61bd46a6d691aa44aaeb2
Juanjo Arranz <juanjose.arranzales@sgs.com>
2019-04-12 17:10:32 +0200

    Big Bug Fixed: fixed a bot initialization once the web part was installed

--- src/webparts/botFrameworkChat/components/BotFrameworkChat.tsx
+++ src/webparts/botFrameworkChat/components/BotFrameworkChat.tsx
@@ -5,9 +5,9 @@ import styles from './BotFrameworkChat.module.scss';
 import { IBotFrameworkChatProps } from './IBotFrameworkChatProps';
 import * as showdown from 'showdown';
 
-declare function require(path: string) : any;
+declare function require( path: string ): any;
 
-export default class BotFrameworkChat extends React.Component<IBotFrameworkChatProps, {}> {
+export default class BotFrameworkChat extends React.Component<IBotFrameworkChatProps, any> {
 
   private pollInterval = 1000;
   private directLineClient;
@@ -17,81 +17,162 @@ export default class BotFrameworkChat extends React.Component<IBotFrameworkChatP
   private currentMessageText;
   private sendAsUserName;
 
-  public render(): JSX.Element {
-    return (
-      <div className={styles.botFrameworkChat}>
-        <div className={styles.container}>
-          <div className={css('ms-Grid-row ms-font-xl', styles.chatHeader)} style={{ backgroundColor: this.props.titleBarBackgroundColor }} >
-            {this.props.title}
-          </div>
-          <div className={css( 'ms-Grid-row', styles.messagesRow )} style={{ height: this.props.messagesRowHeight }}>
-            <div className='ms-Grid-col ms-u-sm12' ref='messageHistoryDiv' dangerouslySetInnerHTML={{ __html: this.getMessagesHtml() }}>
-            </div>
-          </div>
-          <div className={css('ms-Grid-row')}>
-            <TextField id='MessageBox' onKeyUp={(e) => this.tbKeyUp(e)} onKeyDown={(e) => this.tbKeyDown(e)}
-              value={this.currentMessageText} placeholder={this.props.placeholderText} className={css('ms-fontSize-m', styles.messageBox)} />
-          </div>
-        </div>
-      </div>
-    );
+  constructor( props, context ) {
+    super( props );
+    this.state = {
+      resolvedError: false,
+      resolvedSuccess: false,
+      swaggerClient: null,
+      message: null,
+      error: ''
+    };
   }
 
-  public componentDidUpdate(prevProps: IBotFrameworkChatProps, prevState: {}, prevContext: any): void {
-    if (this.props.directLineSecret !== prevProps.directLineSecret) {
-      if (this.props.directLineSecret) {
-        var Swagger = require('swagger-client');
-        var directLineSpec = require('./directline-swagger.json');
+  public render(): JSX.Element {
 
-        this.directLineClientSwagger = new Swagger(
-          {
-            spec: directLineSpec,
-            usePromise: true,
-          }).then((client) => {
-            client.clientAuthorizations.add('AuthorizationBotConnector', new Swagger.ApiKeyAuthorization('Authorization', 'BotConnector ' + this.props.directLineSecret, 'header'));
-            console.log('DirectLine client generated');
-            return client;
-          }).catch((err) =>
-            console.error('Error initializing DirectLine client', err));
+    if ( this.state.resolveError ) {
+      return (
+        <div className={styles.botFrameworkChat}>
+          <div className={styles.container}>
+            <div className={css( 'ms-Grid-row ms-font-xl', styles.chatHeader )} style={{ backgroundColor: this.props.titleBarBackgroundColor }} >
+              {this.props.title}
+            </div>
 
-        this.directLineClientSwagger.then((client) => {
-          client.Conversations.Conversations_NewConversation()
-            .then((response) => response.obj.conversationId)
-            .then((conversationId) => {
+            <div className={css( 'ms-Grid-row' )}>
+              <h3>Error Encountered:{this.state.error} </h3>
+            </div>
+          </div>
+        </div>
+      );
 
-              this.conversationId = conversationId;
-              this.pollMessages(client, conversationId);
-              this.directLineClient = client;
-            });
-        });
+    }
 
-        this.sendAsUserName = this.props.context.pageContext.user.loginName;
+    if ( this.state.resolvedSuccess ) {
+      return (
+        <div className={styles.botFrameworkChat}>
+          <div className={styles.container}>
+            <div className={css( 'ms-Grid-row ms-font-xl', styles.chatHeader )} style={{ backgroundColor: this.props.titleBarBackgroundColor }} >
+              {this.props.title}
+            </div>
+            <div className={css( 'ms-Grid-row', styles.messagesRow )} style={{ height: this.props.messagesRowHeight }}>
+              <div className='ms-Grid-col ms-u-sm12' ref='messageHistoryDiv' dangerouslySetInnerHTML={{ __html: this.getMessagesHtml() }}>
+              </div>
+            </div>
+            <div className={css( 'ms-Grid-row' )}>
+              <TextField id='MessageBox' onKeyUp={( e ) => this.tbKeyUp( e )} onKeyDown={( e ) => this.tbKeyDown( e )}
+                value={this.currentMessageText} placeholder={this.props.placeholderText} className={css( 'ms-fontSize-m', styles.messageBox )} />
+            </div>
+          </div>
+        </div>
+      );
+    }
 
-        this.printMessage = this.printMessage.bind(this);
+    if ( this.props.directLineSecret === '' ) {
+      return (
+        <div className={styles.botFrameworkChat}>
+          <div className={styles.container}>
+            <div className={css( 'ms-Grid-row ms-font-xl', styles.chatHeader )} style={{ backgroundColor: this.props.titleBarBackgroundColor }} >
+              {this.props.title}
+            </div>
+            <div className={css( 'ms-Grid-row' )}>
+              <h3>Enter the Bot Direct Line Secret in the web part properties</h3>
+            </div>
+          </div>
+        </div>
+      );
+    }
+
+    return <h6>...</h6>;
+  }
+
+  public componentDidMount(): void {
+    console.log( 'component mounted' );
+    if ( this.props.directLineSecret ) {
+      if ( !this.state.swaggerClient ) {
+        this._initSwaggerClient();
+
+      } else {
+        this.setState( {
+          resolvedSuccess: true
+        } );
       }
     }
+  }
+
+  public componentDidUpdate( prevProps: IBotFrameworkChatProps, prevState: {}, prevContext: any ): void {
+    console.log( 'component updated' );
+    if ( this.props.directLineSecret ) {
+      if ( !this.state.swaggerClient ) {
+        this._initSwaggerClient();
+      }
+    }
+  }
+
+  private _initSwaggerClient() {
+    this._getSwaggerClient()
+      .then( client => {
+        client.Conversations.Conversations_NewConversation()
+          .then( ( response ) => response.obj.conversationId )
+          .then( ( conversationId ) => {
+
+            this.conversationId = conversationId;
+            this.pollMessages( client, conversationId );
+            this.directLineClient = client;
+          } );
+
+        this.sendAsUserName = this.props.context.pageContext.user.loginName;
+        this.printMessage = this.printMessage.bind( this );
+
+        this.setState( {
+          swaggerClient: client,
+          resolvedSuccess: true
+        } );
+
+      } )
+      .catch( error => { } );
+  }
+
+  private _getSwaggerClient(): Promise<any> {
+    var Swagger = require( 'swagger-client' );
+    var directLineSpec = require( './directline-swagger.json' );
+
+    return new Promise( ( resolve, reject ) => {
+      this.directLineClientSwagger = new Swagger( {
+        spec: directLineSpec,
+        usePromise: true,
+      } ).then( ( client ) => {
+        client.clientAuthorizations.add( 'AuthorizationBotConnector', new Swagger.ApiKeyAuthorization( 'Authorization', 'BotConnector ' + this.props.directLineSecret, 'header' ) );
+        console.log( 'DirectLine client generated' );
+
+        //return client;
+        resolve( client );
+      } ).catch( ( err ) => {
+        console.error( 'Error initializing DirectLine client', err );
+        reject( err );
+      } );
+    } );
   }
 
   public getMessagesHtml() {
     return this.messagesHtml;
   }
 
-  public tbKeyUp(e) {
+  public tbKeyUp( e ) {
     this.currentMessageText = e.target.value;
     this.forceMessagesContainerScroll();
   }
 
-  public tbKeyDown(e) {
-    if (e.keyCode === 13) {
+  public tbKeyDown( e ) {
+    if ( e.keyCode === 13 ) {
       var messageToSend = this.currentMessageText;
 
       this.currentMessageText = '';
 
-      this.setState({
+      this.setState( {
         message: '',
-      });
+      } );
 
-      if (!this.messagesHtml) {
+      if ( !this.messagesHtml ) {
         this.messagesHtml = '';
       }
 
@@ -106,39 +187,39 @@ export default class BotFrameworkChat extends React.Component<IBotFrameworkChatP
             from: this.sendAsUserName,
             text: messageToSend
           }
-        }).catch((err) => console.error('Error sending message:', err));
+        } ).catch( ( err ) => console.error( 'Error sending message:', err ) );
     }
   }
 
-  protected pollMessages(client, conversationId) {
-    console.log('Starting polling message for conversationId: ' + conversationId);
+  protected pollMessages( client, conversationId ) {
+    console.log( 'Starting polling message for conversationId: ' + conversationId );
     var watermark = null;
-    setInterval(() => {
-      client.Conversations.Conversations_GetMessages({ conversationId: conversationId, watermark: watermark })
-        .then((response) => {
+    setInterval( () => {
+      client.Conversations.Conversations_GetMessages( { conversationId: conversationId, watermark: watermark } )
+        .then( ( response ) => {
           watermark = response.obj.watermark;
           return response.obj.messages;
-        })
-        .then((messages) => this.printMessages(messages));
-    }, this.pollInterval);
+        } )
+        .then( ( messages ) => this.printMessages( messages ) );
+    }, this.pollInterval );
   }
 
-  protected printMessages(messages) {
-    if (messages && messages.length) {
-      messages = messages.filter((m) => m.from !== this.sendAsUserName);
-      if (messages.length) {
-        messages.forEach(this.printMessage);
+  protected printMessages( messages ) {
+    if ( messages && messages.length ) {
+      messages = messages.filter( ( m ) => m.from !== this.sendAsUserName );
+      if ( messages.length ) {
+        messages.forEach( this.printMessage );
       }
     }
   }
 
-  protected printMessage(message) {
-    if (message.text) {
-      this.setState({
-        message: this.currentMessageText
-      });
+  protected printMessage( message ) {
+    if ( message.text ) {
+      this.setState( {
+        message: message.text
+      } );
 
-      if (!this.messagesHtml) {
+      if ( !this.messagesHtml ) {
         this.messagesHtml = '';
       }
 
@@ -149,14 +230,14 @@ export default class BotFrameworkChat extends React.Component<IBotFrameworkChatP
       // get all links that are not html-ready yet and convert them to html
       let regex: RegExp = /[^"'](https:\/\/[^\s]+)/g;
       if ( regex.test( answerHtml ) ) {
-        let linksToHtml = answerHtml.match( regex ).map( s => s.replace( /[^h]*/, '' ));
-        linksToHtml.forEach( link => answerHtml = answerHtml.replace( link, `<a href="${link}">${link}</a>` ) );
+        let linksToHtml = answerHtml.match( regex ).map( s => s.replace( /[^h]*/, '' ) );
+        linksToHtml.forEach( link => answerHtml = answerHtml.replace( link, `<a href="${ link }">${ link }</a>` ) );
       }
 
       this.messagesHtml +=
         `<span class = "${ styles.message } ${ styles.fromBot } ms-fontSize-m"
           style="background-color: ${this.props.botMessagesBackgroundColor }; color: ${ this.props.botMessagesForegroundColor }">
-            ${answerHtml}
+            ${answerHtml }
         </span>`;
 
       this.forceUpdate();
@@ -167,7 +248,7 @@ export default class BotFrameworkChat extends React.Component<IBotFrameworkChatP
 
   protected forceMessagesContainerScroll() {
     var messagesRowClass = '.' + styles.messagesRow;
-    var messagesDivElement = document.querySelector(messagesRowClass);
+    var messagesDivElement = document.querySelector( messagesRowClass );
     messagesDivElement.scrollTop = messagesDivElement.scrollHeight;
   }
 
